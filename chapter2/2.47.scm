80840f1ab5d0812555742a1afef03da6742c2f19
chenyonghao <chenyonghao6717@yaocheng.cn>
2026-01-24 14:17:56 +0800

    finish 2.47.scm

---
+++ chapter2/2.47.scm
@@ -0,0 +1,13 @@
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (get-orgin frame) (car frame))
+(define (get-edge1 frame) (cadr frame))
+(define (get-edge2 frame) (caddr frame))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (get-orgin frame) (car frame))
+(define (get-edge1 frame) (cadr frame))
+(define (get-edge2 frame) (cddr frame))
