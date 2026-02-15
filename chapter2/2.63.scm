7b7d67c3627221ceb98579a13bb7aadc36a96050
chenyonghao <cyh@chenyonghaos-MacBook-Air.local>
2026-02-15 15:18:25 +0800

    finish 2.61.scm 2.62.scm 2.63.scm

---
+++ chapter2/2.63.scm
@@ -0,0 +1,4 @@
+; a. The 2 procedures generate the same output list. The produced list of fig. 2.16 is (1 3 5 7 9 11)
+
+; b. The second procedure is faster because it only uses cons(O(1)) at each node and visits each node once.
+; The time complexity is O(n) if the tree has n nodes
